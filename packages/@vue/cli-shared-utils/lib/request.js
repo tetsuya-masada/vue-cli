3a3cfc330116c8489040d9736ddc2726f2fef8e2
tetsuya-masada <tetsuya-masada@isecprovide.com>
2024-08-05 11:35:13 +0900

    fix(cli-shared-utils): replace request with axios to resolve security vulnerability

--- packages/@vue/cli-shared-utils/lib/request.js
+++ packages/@vue/cli-shared-utils/lib/request.js
@@ -1,13 +1,13 @@
+const axios = require('axios')
+
 exports.request = {
   get (url, opts) {
-    // lazy require
-    const fetch = require('node-fetch')
     const reqOpts = {
       method: 'GET',
       timeout: 30000,
       ...opts
     }
 
-    return fetch(url, reqOpts).then(result => result.json())
+    return axios.get(url, reqOpts).then(response => response.data)
   }
 }
